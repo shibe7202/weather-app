8ed24fbe8a6becfba5de3a57101e8cfe3879f6bc
shibe7202 <46942349+shibe7202@users.noreply.github.com>
2024-09-09 15:18:46 -0700

    Write functions to access API and process JSON.
    getWeatherData is used to fetch from the API. processData will process
    the JSON data and return an object with weather information.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
 import './style.css'
 import './magnify.svg'
+
+let weatherData
+const key = '6CTDN5Z7DR4SGMELQYE6CH37R'
+
+const updateDisplay = function updateDisplayDOM() {}
+
+const getWeatherData = async function fetchWeatherDataObject() {
+    try {
+        const response = await fetch(
+            'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/sandiego/?key=' +
+                key,
+            { mode: 'cors' }
+        )
+        if (response.status == 200) {
+            weatherData = response
+            processData()
+        }
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+const processData = async function processJSONData() {
+    const newData = await weatherData.json()
+    console.log(newData)
+}
+
+getWeatherData()
